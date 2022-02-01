572b1f2111e7857dd8c995aa98f2295a3143db85
Sovan22 <deepsin059@gmail.com>
2022-02-01 14:39:10 +0530

    Update handler.js

--- handler.js
+++ handler.js
@@ -309,7 +309,7 @@ module.exports = {
                 console.error(e)
               }
             }
-            if (m.limit) m.reply(+ m.limit + ' Limit terpakai')
+            if (m.limit) m.reply(+ m.limit + ' Limit used')
           }
           break
         }
